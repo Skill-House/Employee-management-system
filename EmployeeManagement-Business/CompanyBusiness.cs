904d0fa7df6843fe734ebf7a8ec98c948261ab1a
Aishwarya <aishwaryaputhran1@gmail.com>
2022-06-07 20:05:17 +0530

    Get and delete functionality

--- EmployeeManagement-Business/CompanyBusiness.cs
+++ EmployeeManagement-Business/CompanyBusiness.cs
@@ -1,6 +1,32 @@
-﻿namespace EmployeeManagement_Business
+﻿using EmployeeManagement_Repository;
+using EmployeeManagement_Repository.Entities;
+using System.Net;
+
+namespace EmployeeManagement_Business
 {
     public class CompanyBusiness
     {
+        private readonly CompanyRepository companyRepository;
+
+        public CompanyBusiness()
+        {
+            this.companyRepository = new CompanyRepository();
+        }
+        public async Task<List<Company>> GetAllCompaniesAsync()
+        {
+            return await companyRepository.GetAllCompaniesAsync();
+        }
+
+        public async Task<Company> GetByID(int id)
+        {
+            return await companyRepository.GetByID(id);
+        }
+
+        public async Task<HttpStatusCode> DeleteByID(int id)
+        {
+            var result = companyRepository.DeleteByID(id);
+            return HttpStatusCode.OK;
+
+        }
     }
 }
