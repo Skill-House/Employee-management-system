83da2d3e0754b8cc99cb407dbae0dc3369d6a95c
Nithin Kumar <nk243716@gmail.com>
2022-06-16 22:02:00 +0530

    Created User and UserRole Table

---
+++ EmployeeManagement-Repository/Entities/UserRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace EmployeeManagement_Repository.Entities
+{
+    public partial class UserRole
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public string UserRole1 { get; set; }
+
+        public virtual User IdNavigation { get; set; }
+    }
+}
