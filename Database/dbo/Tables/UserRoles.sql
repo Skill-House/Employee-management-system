b97ba9a9f7053a4dc2799e32074fc48ebc6ec98d
Nithin Kumar <nk243716@gmail.com>
2022-06-16 22:06:08 +0530

    Schema Compared Update

---
+++ Database/dbo/Tables/UserRoles.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[UserRoles] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]   NVARCHAR (50) NOT NULL,
+    [UserRole] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_UserRoles_1] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_UserRoles_UserRoles] FOREIGN KEY ([Id]) REFERENCES [dbo].[Users] ([Id])
+);
+
---
+++ Database/dbo/Tables/Users.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Users] (
+    [Id]       INT           IDENTITY (1, 1) NOT NULL,
+    [UserId]   NVARCHAR (50) NOT NULL,
+    [Password] NVARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Users_1] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
