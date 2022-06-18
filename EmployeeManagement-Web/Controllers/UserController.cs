e25f78f357383cec37c806d6f4fcd274caf80cfb
Aishwarya <aishwaryaputhran1@gmail.com>
2022-06-18 19:22:39 +0530

    Login Validation

---
+++ EmployeeManagement-Web/Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using EmployeeManagement.Data.Models;
+using EmployeeManagement_Business;
+using EmployeeManagement_Repository.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace EmployeeManagement_Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController: Controller
+    {
+
+            private readonly UserBusiness userBusiness;
+
+            public UserController()
+            {
+                userBusiness = new UserBusiness();
+            }
+
+
+         [HttpPost("ValidateUser")]
+        public async Task<IActionResult> ValidateUser(UserModel userModel)
+        {
+            var existingUser = await userBusiness.ValidateUser(userModel);
+
+            if (existingUser == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+
+        }
+    }
+}
