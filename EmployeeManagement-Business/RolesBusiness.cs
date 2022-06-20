476ad9a7e6d6c2bafb8a575b7281551596c1cd5e
Bhimasi <bhimasi.bhajantri@aykansoft.com>
2022-06-20 18:47:21 +0530

    skiped roll crud operations

--- EmployeeManagement-Business/RolesBusiness.cs
+++ EmployeeManagement-Business/RolesBusiness.cs
@@ -1,60 +1,60 @@
-﻿using EmployeeManagement.Data.Models;
-using EmployeeManagement_Repository;
-using EmployeeManagement_Repository.Entities;
+﻿//using EmployeeManagement.Data.Models;
+//using EmployeeManagement_Repository;
+//using EmployeeManagement_Repository.Entities;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Net;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace EmployeeManagement_Business
-{
-    public class RolesBusiness
-    {
-        private readonly RolesRepository rolesRepository;
+//namespace EmployeeManagement_Business
+//{
+//    public class RolesBusiness
+//    {
+//        private readonly RolesRepository rolesRepository;
 
-        public RolesBusiness()
-        {
-            rolesRepository = new RolesRepository();
-        }
+//        public RolesBusiness()
+//        {
+//            rolesRepository = new RolesRepository();
+//        }
 
-        public async Task<List<RolesModel>> GetRolesAsync()
-        {
+//        public async Task<List<RolesModel>> GetRolesAsync()
+//        {
           
-             return await rolesRepository.GetRolesAsync();
+//             return await rolesRepository.GetRolesAsync();
 
-        }
+//        }
 
-        public async Task<RolesModel> GetRolesByID(int Id)
-        {
-            return await rolesRepository.GetRolesByID(Id);
-        }
+//        public async Task<RolesModel> GetRolesByID(int Id)
+//        {
+//            return await rolesRepository.GetRolesByID(Id);
+//        }
 
-        public async Task DeleteRolesByID(int Id)
-        {
-            await rolesRepository.DeleteRolesByID(Id);
-        }
+//        public async Task DeleteRolesByID(int Id)
+//        {
+//            await rolesRepository.DeleteRolesByID(Id);
+//        }
 
-        public async Task<HttpStatusCode> CreateRoles(CreateRolesModel createRolesModel)
-        {
-            var newRole = new Role();
-            newRole.RollName = createRolesModel.RollName;
-             rolesRepository.CreateRoles(newRole);
-            return HttpStatusCode.OK;
+//        public async Task<HttpStatusCode> CreateRoles(CreateRolesModel createRolesModel)
+//        {
+//            var newRole = new Role();
+//            newRole.RollName = createRolesModel.RollName;
+//             rolesRepository.CreateRoles(newRole);
+//            return HttpStatusCode.OK;
 
-        }
+//        }
 
-        public async Task<HttpStatusCode> UpdateRolesAsync(RolesModel rolesModel)
-        {
-            var updatedrole = new Role();
-            updatedrole.Id = rolesModel.Id;
-            updatedrole.RollName = rolesModel.RollName;
-            await rolesRepository.UpdateRoles(updatedrole);
-            return HttpStatusCode.OK;
+//        public async Task<HttpStatusCode> UpdateRolesAsync(RolesModel rolesModel)
+//        {
+//            var updatedrole = new Role();
+//            updatedrole.Id = rolesModel.Id;
+//            updatedrole.RollName = rolesModel.RollName;
+//            await rolesRepository.UpdateRoles(updatedrole);
+//            return HttpStatusCode.OK;
 
 
-        }
-}
-    }
+//        }
+//}
+//    }
--- EmployeeManagement-Repository/RolesRepository.cs
+++ EmployeeManagement-Repository/RolesRepository.cs
@@ -1,78 +1,78 @@
-﻿using EmployeeManagement_Repository.Entities;
-using EmployeeManagement.Data.Models;
+﻿//using EmployeeManagement_Repository.Entities;
+//using EmployeeManagement.Data.Models;
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using Microsoft.EntityFrameworkCore;
 
-namespace EmployeeManagement_Repository
-{
-    public class RolesRepository
-    {
+//namespace EmployeeManagement_Repository
+//{
+//    public class RolesRepository
+//    {
 
-        private readonly EmployeeManagementContext dbContext;
+//        private readonly EmployeeManagementContext dbContext;
 
-        public RolesRepository()
-        {
-            dbContext = new EmployeeManagementContext();
-        }
+//        public RolesRepository()
+//        {
+//            dbContext = new EmployeeManagementContext();
+//        }
 
-        public async Task<List<RolesModel>> GetRolesAsync()
-        {
-            var result = dbContext.Roles.Include(x => x.Users).ToList();
-            var roleDetails = new List<RolesModel>();
-            foreach (var role in result)
-            {
-                roleDetails.Add(new RolesModel()
-                {
-                   Id = role.Id,
-                   RollName = role.RollName,
-                });
-            }
-            return roleDetails;
-        }
+//        public async Task<List<RolesModel>> GetRolesAsync()
+//        {
+//            var result = dbContext.Roles.Include(x => x.Users).ToList();
+//            var roleDetails = new List<RolesModel>();
+//            foreach (var role in result)
+//            {
+//                roleDetails.Add(new RolesModel()
+//                {
+//                   Id = role.Id,
+//                   RollName = role.RollName,
+//                });
+//            }
+//            return roleDetails;
+//        }
 
-        public async Task<RolesModel> GetRolesByID(int Id)
-        {
+//        public async Task<RolesModel> GetRolesByID(int Id)
+//        {
 
-            var result =   dbContext.Roles.FirstOrDefault(a => a.Id == Id);
-            var  rolesdeatail = new RolesModel();
-            rolesdeatail.Id = result.Id;
-            rolesdeatail.RollName = result.RollName;
-            return rolesdeatail;
-        }
+//            var result =   dbContext.Roles.FirstOrDefault(a => a.Id == Id);
+//            var  rolesdeatail = new RolesModel();
+//            rolesdeatail.Id = result.Id;
+//            rolesdeatail.RollName = result.RollName;
+//            return rolesdeatail;
+//        }
 
-        public async Task DeleteRolesByID(int Id)
-        {
-            var DelRole =  dbContext.Roles.FirstOrDefault(a => a.Id == Id);
-            if (DelRole != null)
-            {
-                dbContext.Remove(DelRole);
-                dbContext.SaveChanges();
+//        public async Task DeleteRolesByID(int Id)
+//        {
+//            var DelRole =  dbContext.Roles.FirstOrDefault(a => a.Id == Id);
+//            if (DelRole != null)
+//            {
+//                dbContext.Remove(DelRole);
+//                dbContext.SaveChanges();
 
-            }
-        }
+//            }
+//        }
 
-        public async Task CreateRoles(Role role)
-        {
-            dbContext.Roles.Add(role);
-            dbContext.SaveChanges();
+//        public async Task CreateRoles(Role role)
+//        {
+//            dbContext.Roles.Add(role);
+//            dbContext.SaveChanges();
 
-        }
+//        }
 
-        public async Task UpdateRoles(Role roles)
-        {
-            var existingRole = dbContext.Roles.Where(h => h.Id == roles.Id).FirstOrDefault();
-            if (existingRole != null)
-            {
-                //existingRole.Id = roles.Id;
-                existingRole.RollName = roles.RollName;
-                  await this.dbContext.SaveChangesAsync();
-            }
-        }
+//        public async Task UpdateRoles(Role roles)
+//        {
+//            var existingRole = dbContext.Roles.Where(h => h.Id == roles.Id).FirstOrDefault();
+//            if (existingRole != null)
+//            {
+//                //existingRole.Id = roles.Id;
+//                existingRole.RollName = roles.RollName;
+//                  await this.dbContext.SaveChangesAsync();
+//            }
+//        }
 
-    }
-}
+//    }
+//}
