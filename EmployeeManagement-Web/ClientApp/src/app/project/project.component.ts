4b572d629c9d152a430555e49bcf40ad71ecfee4
vidyashrihunsikatti <vidyashrihunsikatti@gmail.com>
2022-07-08 16:32:57 +0530

    Server

--- EmployeeManagement-Web/ClientApp/src/app/project/project.component.ts
+++ EmployeeManagement-Web/ClientApp/src/app/project/project.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { AdminService } from '../admin/services/admin.service';
-
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
   styleUrls: ['./project.component.css']
 })
-export class ProjectComponent implements OnInit {
+export class ProjectComponent implements OnInit 
+{
   projectData: any;
-  EditData: any;
-
+  router: any;
 
   constructor(private adminService:AdminService) {
     this.getAllProject();
@@ -26,13 +26,13 @@ export class ProjectComponent implements OnInit {
     }
     )
   }
-editAllProject(){
-  this.adminService.editAllProject().subscribe((p: any)=>
-  {
-    debugger;
-    this.EditData = p;
-  }
-  )
+ editAllProjects()
+ {
+  debugger;
+  this.router.navigate(["Project/edit-project"])
 
+ }
 }
-}
+
+
+
